d1105c548e07369cd4e7127339868a4b54021a84
Trần Ngọc Huy <truongduchuy910@gmail.com>
2023-11-21 16:15:30 +0700

    feat: add packge @semantic-release/git

--- release.config.mjs
+++ release.config.mjs
@@ -16,5 +16,13 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
