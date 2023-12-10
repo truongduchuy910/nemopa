6724aedc331b617203f93d4487eaef857506cdcf
Trần Ngọc Huy <truongduchuy910@gmail.com>
2023-12-10 14:47:33 +0700

    fix: Add README to assets

--- release.config.mjs
+++ release.config.mjs
@@ -24,7 +24,7 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"],
+        assets: ["package.json", "README.md"],
         message:
           "chore(release): ${nextRelease.version} 🚀\n\n${nextRelease.notes}",
       },
